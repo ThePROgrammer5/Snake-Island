313481468fd0b2fb092a30cbb11bbbc958c2bd05
The PROgrammer / <codingplusgaming@gmail.com>
2022-06-18 19:44:13 -0700

    done

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
 
-ctx.drawImage('snakeHead.png', 0, 0, 100, 100, 0, 0, 100, 100);
-
-ctx.drawImage('snakeBody.png', 0, 0, 100, 100, 0, 0, 100, 100);
+body = new Image();
+  body.src = 'snakeBody.png';
+  body.onload = function(){
+    context.drawImage(body, 0, 0)
+};
